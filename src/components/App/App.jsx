1dd1150030e9b77fce96ff87efde14012b9fc039
AndrewEhrhardt <andyehrhardt@gmail.com>
2021-09-26 14:29:08 -0500

    completed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import ReactDOM from 'react-dom';
 import axios from 'axios';
 import PageOne from '../PageOne/PageOne.jsx'
 import PageTwo from '../PageTwo/PageTwo'
@@ -9,11 +8,8 @@ import Review from '../Review/Review'
 import ThankYou from '../ThankYou/ThankYou'
 import Admin from '../Admin/Admin'
 import Box from '@mui/material/Box';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import Paper from '@mui/material/Paper';
-
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-
+import { HashRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
@@ -29,8 +25,6 @@ function App() {
       console.log(error);
     })
   }
-  
-
   return (
     <div className='App'>
       <header className='App-header'>
